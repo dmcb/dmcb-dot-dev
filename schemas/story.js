efb05cf1bfbdf37e4dd13c6042adc1b11aba2202
Derek McBurney <d.mcburney@icloud.com>
2022-09-12 12:18:40 -0600

    Adds Sanity with placeholder schema

---
+++ schemas/story.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'story',
+  type: 'document',
+  title: 'Story',
+  fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      }
+  ]
+}
