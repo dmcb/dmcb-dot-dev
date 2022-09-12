08748ae812937fcaf5cde1b0ffa80dee83cee6f1
Derek McBurney <d.mcburney@icloud.com>
2022-09-12 14:11:04 -0600

    Adds site settings content type and makes it unique

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,12 +5,14 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // My schema types
+import siteSettings from './siteSettings'
 import story from './story'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
+    siteSettings,
     story
   ]),
 })
---
+++ schemas/siteSettings.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'siteSettings',
+  type: 'document',
+  title: 'Site Settings',
+  fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Site Title'
+      }
+  ]
+}
