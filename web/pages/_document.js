d86e73efb64c1e8a3ba603bb5f99f6b21967c70f
dmcb <d.mcburney@icloud.com>
2023-04-17 15:41:46 -0600

    Removes Google fonts

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -6,10 +6,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/icon.png" type="image/png" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css" />
         </Head>
         <body>
           <Main />
