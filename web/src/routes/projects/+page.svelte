453337a48815f3a82f9da7b082fd59d6324dc9e6
Derek McBurney <d.mcburney@icloud.com>
2024-05-21 10:08:45 -0600

    Updates styles

--- web/src/routes/projects/+page.svelte
+++ web/src/routes/projects/+page.svelte
@@ -16,14 +16,19 @@
       margin-left: -2em;
       margin-right: -2em;
       padding: 1.5em 2em;
-      border: 1px solid #ddd;
+      background-color: #eee;
 
       h2 {
         margin-top: 0;
       }
 
       @media (prefers-color-scheme: dark) {
-        border-color: #333;
+        background-color: #222;
+      }
+    }
+    @media only screen and (min-width: 720px) {
+      .content {
+        font-size: 0.9em;
       }
     }
   }
@@ -96,7 +101,7 @@
               {#if project.github}
                 <a href="{project.github}" class="button" target="_blank">
                   <div>
-                    <span>GitHub repo</span>
+                    <span>GitHub</span>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                       <path fill="#999" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
                     </svg>
