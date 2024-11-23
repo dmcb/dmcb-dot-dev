4950fd770d27e8dbe24dc2053081ef319d33ee2c
Derek McBurney <d.mcburney@icloud.com>
2024-11-23 14:33:07 -0700

    Updates nav to use Bluesky, kills projects for now

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -434,9 +434,9 @@
           <!-- <li class={$page.url.pathname == "/on-tap" ? "active" : ""}>
             <a on:click={() => {navExpanded = false}} on:focus={() => {navExpanded = true}} on:blur={() => { navExpanded = false }} href="/on-tap">What's On Tap</a>
           </li> -->
-          <li class={$page.url.pathname == "/projects" ? "active" : ""}>
+          <!-- <li class={$page.url.pathname == "/projects" ? "active" : ""}>
             <a on:click={() => {navExpanded = false}} on:focus={() => {navExpanded = true}} on:blur={() => { navExpanded = false }} href="/projects">Projects</a>
-          </li>
+          </li> -->
           <li>
             <a on:click={() => {navExpanded = false}} on:focus={() => {navExpanded = true}} on:blur={() => { navExpanded = false }} href="https://medium.com/@d.mcburney">Writing</a>
           </li>
@@ -447,8 +447,8 @@
         <ul class="icons">
           <li>
             <a on:click={() => {navExpanded = false}} on:focus={() => {navExpanded = true}} on:blur={() => { navExpanded = false }} href="https://hachyderm.io/@dmcb" title="Mastodon" target="_blank" rel="me">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 417.8 512" >
-                <path xmlns="http://www.w3.org/2000/svg" d="M417.8 179.1c0-97.2-63.7-125.7-63.7-125.7-62.5-28.7-228.5-28.4-290.4 0 0 0-63.7 28.5-63.7 125.7 0 115.7-6.6 259.4 105.6 289.1 40.5 10.7 75.3 13 103.3 11.4 50.8-2.8 79.3-18.1 79.3-18.1l-1.7-36.9s-36.3 11.4-77.1 10.1c-40.4-1.4-83-4.4-89.6-54-.6-4.4-.9-9-.9-13.9 85.6 20.9 158.6 9.1 178.7 6.7 56.1-6.7 105-41.3 111.2-72.9 9.8-49.8 9-121.5 9-121.5zm-75.1 125.2h-46.6V190.1c0-49.7-64-51.6-64 6.9v62.5h-46.3V197c0-58.5-64-56.6-64-6.9v114.2H75.1c0-122.1-5.2-147.9 18.4-175 25.9-28.9 79.8-30.8 103.8 6.1l11.6 19.5 11.6-19.5c24.1-37.1 78.1-34.8 103.8-6.1 23.7 27.3 18.4 53 18.4 175z"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path fill="#999" d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565C.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479c.815 2.736 3.713 3.66 6.383 3.364c.136-.02.275-.039.415-.056c-.138.022-.276.04-.415.056c-3.912.58-7.387 2.005-2.83 7.078c5.013 5.19 6.87-1.113 7.823-4.308c.953 3.195 2.05 9.271 7.733 4.308c4.267-4.308 1.172-6.498-2.74-7.078a8.741 8.741 0 0 1-.415-.056c.14.017.279.036.415.056c2.67.297 5.568-.628 6.383-3.364c.246-.828.624-5.79.624-6.478c0-.69-.139-1.861-.902-2.206c-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8"></path>
               </svg>
             </a>
           </li>
