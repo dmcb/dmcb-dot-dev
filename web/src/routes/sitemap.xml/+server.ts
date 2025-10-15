bac28f4cda69c8c093aa6d607af115078ff4165c
dmcb <d.mcburney@icloud.com>
2025-10-14 23:46:47 -0600

    Pre-renders sitemap

--- web/src/routes/sitemap.xml/+server.ts
+++ web/src/routes/sitemap.xml/+server.ts
@@ -1,5 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
 	const lastMod = new Date().toISOString();
 
